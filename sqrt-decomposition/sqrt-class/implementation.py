d0154cc725982e2eb714351b39ac5457daae718d
SorryGames <mdraevich@solidex.by>
2020-03-01 12:46:14 +0300

    01.03.2020 12:46

--- sqrt-decomposition/sqrt-class/implementation.py
+++ sqrt-decomposition/sqrt-class/implementation.py
@@ -1,17 +1,18 @@
 import math
 
+
 class SqrtDecomposition:
 
-    def __init__(self, array, func=lambda a : sum(a)):
+    def __init__(self, array, func=lambda a: sum(a)):
         self.array = array
-        self.sqrt_array = self.build(array, func) 
+        self.sqrt_array = self.build(array, func)
 
     def build(self, array, func):
         n = len(array)
         m = int(math.sqrt(n))
         temp = []
 
-        for i in range(0, n, m): 
+        for i in range(0, n, m):
             temp.append(func(array[i:i+m]))
 
         return temp
