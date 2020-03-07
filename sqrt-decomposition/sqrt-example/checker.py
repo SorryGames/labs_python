b60ab076c126340485be5d69e98053ef123f3d58
SorryGames <mdraevich@solidex.by>
2020-03-07 09:31:25 +0300

    07:03:2020 09:31

---
+++ sqrt-decomposition/sqrt-example/checker.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append("/home/matvey/Desktop/python/sqrt-decomposition/sqrt-class")
+from implementation import SqrtDecomposition
+
+
+class Checker: 
+
+    def __init__(self, test_count):
+        self.test_count = test_count
+
+    def generate(self):
+        pass
+
+    def sum(self, l, r): 
+        pass
+
+    def add(self, l, r):
+        pass
+
+    def assign(self, l, r): 
+        pass
