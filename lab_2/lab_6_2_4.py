7ea07ba29947d0c3d57ea693829842b2cf505cb7
SorryGames <mdraevich@solidex.by>
2020-05-03 00:12:08 +0300

    task4 00:12

--- lab_2/lab_6_2_4.py
+++ lab_2/lab_6_2_4.py
@@ -3,10 +3,33 @@
 import sys
 
 
-def flatten_it(array):
-	pass
+def _flatten_it(data, history):
+	history.append(id(data))
+	#
+	for element in data:
+		if id(element) in history:
+			yield None
+		elif isinstance(data, dict):
+			if isinstance(data[element], dict):
+				yield from _flatten_it(data[element], history)
+			else:
+				yield {element: data[element]}
+		elif isinstance(element, (list, dict, tuple)):
+			try:
+				yield from _flatten_it(element, history)
+			except:
+				yield None
+		else:
+			yield element
+	history.pop()
+
+
+def flatten_it(data):
+	return _flatten_it(data, [])
 
 
 if __name__ == "__main__":
-	data = [1, 2, 3, [4, 5], [1], []]
-	
+	data = {1: {2: 4, 3: {3: 6, 4: 8}}, 5: 10}
+	print(data)
+	#
+	print(list(flatten_it(data)))
