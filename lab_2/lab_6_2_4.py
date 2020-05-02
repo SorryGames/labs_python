f1377b6f9b0b0b9ad0ccf40bac0d556a5057194f
SorryGames <mdraevich@solidex.by>
2020-05-03 00:29:10 +0300

    task4 00:12

--- lab_2/lab_6_2_4.py
+++ lab_2/lab_6_2_4.py
@@ -29,7 +29,24 @@ def flatten_it(data):
 
 
 if __name__ == "__main__":
-	data = {1: {2: 4, 3: {3: 6, 4: 8}}, 5: 10}
-	print(data)
+	cycle_1 = [1, 2, [4, 5]]
+	cycle_1[2].append(cycle_1)
+	cycle_1.append(cycle_1)
 	#
-	print(list(flatten_it(data)))
+	cycle_2 = [3, [4]]
+	cycle_2[1].append(cycle_2)
+	cycle_2[1][1].append(cycle_1)
+	#
+	tests = [
+		{1: {2: 4, 3: {3: 6, 4: 8}}, 5: 10},
+		[1, 2, [[3, 4], 5], [6]], 
+		(1, 2, (3, 4), (5, ), (6, (7, 8, (9, )))),
+		cycle_1, 
+		cycle_2
+	]
+	#
+	for data in tests:
+		print("test:", data)
+		#
+		answer = list(flatten_it(data))
+		print(answer, end="\n\n")
