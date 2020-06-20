e2b23be4fe499130ed7191c551a76b7482798210
SorryGames <mdraevich@solidex.by>
2020-06-20 12:37:12 +0300

    fixes

--- RMO/test_mro.py
+++ RMO/test_mro.py
@@ -7,10 +7,13 @@ class C: pass
 class B(C): pass
 class D(B, A): pass
 class E(D, A): pass
+class F(E, B, A): pass
+class G(F, E, D, B): pass
+class K(G, F, E, D, B): pass
 
 
-print(E.__mro__)
-print(own_mro(E))
+print(K.__mro__)
+print(own_mro(K))
 
 print()
-print(E.__mro__ == own_mro(E))
+print(K.__mro__ == own_mro(K))
